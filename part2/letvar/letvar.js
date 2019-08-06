d2a6f96ad602fe5b5c3ac4d0032d175ae9d46607
y.t <y.softvalley@gmail.com>
2019-08-07 00:20:44 +0900

    tsc

--- part2/letvar/letvar.js
+++ part2/letvar/letvar.js
@@ -29,14 +29,14 @@
     console.log(data5); // エラーにならない
 })();
 (function () {
-    var mail = 'admin@example.com';
-    var msg = "\u30EC\u30D3\u30E5\u30A2\u30FC\u52DF\u96C6\u4E2D!\n    \u66F8\u7C4D\u306E\u3054\u611F\u60F3\u3092\u305C\u3072\u304A\u805E\u304B\u305B\u304F\u3060\u3055\u3044\u3002\n    \u5F0A\u793E\u30B5\u30DD\u30FC\u30C8\u30B5\u30A4\u30C8\u3001SNS\u306A\u3069\u3067\u7D39\u4ECB\u3055\u305B\u3066\u3044\u305F\u3060\u304D\u307E\u3059\u3002\n    (\u9001\u4ED8\u5148 : " + mail + ")"; // テンプレート文字列 & 埋め込み可能
-    console.log(msg);
-})();
-(function () {
     var data; // any 型
     data = 10.5; // number 型とみなされる
     console.log(data.toFixed(0)); // number 型だから呼ぶことができる
     data = 'hoge'; // string 型とみなされる
     console.log(data.charAt(0)); // string 型だから呼ぶことができる
+})();
+(function () {
+    var mail = 'admin@example.com';
+    var msg = "\u30EC\u30D3\u30E5\u30A2\u30FC\u52DF\u96C6\u4E2D!\n    \u66F8\u7C4D\u306E\u3054\u611F\u60F3\u3092\u305C\u3072\u304A\u805E\u304B\u305B\u304F\u3060\u3055\u3044\u3002\n    \u5F0A\u793E\u30B5\u30DD\u30FC\u30C8\u30B5\u30A4\u30C8\u3001SNS\u306A\u3069\u3067\u7D39\u4ECB\u3055\u305B\u3066\u3044\u305F\u3060\u304D\u307E\u3059\u3002\n    (\u9001\u4ED8\u5148 : " + mail + ")"; // テンプレート文字列 & 埋め込み可能
+    console.log(msg);
 })();
