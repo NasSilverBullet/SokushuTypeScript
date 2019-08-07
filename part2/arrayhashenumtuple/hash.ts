d4ea0f0e3bd17ba7c0fce770f9c7c270a18e4fd7
y.t <y.softvalley@gmail.com>
2019-08-07 21:40:16 +0900

    finidh hash part

--- part2/arrayhashenumtuple/hash.ts
+++ part2/arrayhashenumtuple/hash.ts
@@ -1,7 +1,21 @@
 { // 連想配列
-    let obj: {[index: string]: string;} = {
+    let obj: {[index: string]: string;} = { // { [key: string]: string;} でも可能
         'hoge': 'ほげ',
         'foo': 'ふぅ',
         'bar': 'ばぁ'
     }
+    console.log(obj.hoge); // プロパティ構文
+    console.log(obj['hoge']); // ブランケット構文
+}
+
+{ // interface を使うことで型として用意する事ができる
+    interface StringMap { // 何度も使う場合は有功
+        [index: string]: string;
+    }
+    let obj: StringMap = {
+        'hoge': 'ほげ',
+        'foo': 'ふぅ',
+        'bar': 'ばぁ'
+    };
+    console.log(obj.hoge);
 }
