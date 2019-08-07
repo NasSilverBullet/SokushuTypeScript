da2a361ce9530b60544d92d774d209d3420732c4
y.t <y.softvalley@gmail.com>
2019-08-07 21:24:58 +0900

    fix file names

--- part2/arrayhashenumtuple/array.ts
+++ part2/arrayhashenumtuple/array.ts
@@ -1,25 +1,30 @@
+// 配列
 { // 配列
-    var data = ['Java', 'Python', 'PHP', 'Ruby', 'Ruby', 'C#'];
+    let data: string[] = ['Java', 'Python', 'PHP', 'Ruby', 'Ruby', 'C#'];
     console.log(data[0]);
 }
+
 { // ジェネリクス(基本は上の配列リテラルを使う)
-    var data = ['Java', 'Python', 'PHP', 'Ruby', 'Ruby', 'C#'];
+    let data: Array<string> = ['Java', 'Python', 'PHP', 'Ruby', 'Ruby', 'C#']
     console.log(data);
 }
+
 { // 行列、テンソルはこのように書く
-    var data = [
+    let data: number[][] = [
         [10, 20],
         [30, 40],
         [50, 60],
     ];
     console.log(data[1][1]); // 40
 }
-{ // Array コンストラクター
-    var data = new Array(5, 10, 15);
+
+{ // Array コンストラクター(わかりにくいので Array コンストラクターは避ける)
+    let data: number[] = new Array(5, 10, 15);
     console.log(data);
 }
+
 { // 要素5、10、15 の配列 (わかりにくいので Array コンストラクターは避ける)
-    var data = new Array(5, 10, 15);
+    let data: number[] = new Array(5, 10, 15);
     console.log(data);
     // let data: number[] = []; 空の配列は空のブランケットで表現
 }
---
+++ part2/arrayhashenumtuple/hash.ts
@@ -0,0 +1,7 @@
+{ // 連想配列
+    let obj: {[index: string]: string;} = {
+        'hoge': 'ほげ',
+        'foo': 'ふぅ',
+        'bar': 'ばぁ'
+    }
+}
