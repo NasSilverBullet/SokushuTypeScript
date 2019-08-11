52af5e969b437c2b7ddb3d164f9c3e9a4b6ba664
y.t <y.softvalley@gmail.com>
2019-08-11 23:23:07 +0900

    finish implements parts

--- part4/extendsimplements/interface.js
+++ part4/extendsimplements/interface.js
@@ -76,3 +76,30 @@
     x.hoge();
     x.foo(); // Property 'foo' does not exist on type '{ hoge(): void; }
 }
+{ // 型としてのthis (メソッドチェーン)
+    var MyClass = /** @class */ (function () {
+        function MyClass(_value) {
+            this._value = _value;
+        }
+        ;
+        Object.defineProperty(MyClass.prototype, "value", {
+            get: function () {
+                return this._value;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        MyClass.prototype.add = function (value) {
+            this._value += value;
+            returnturn;
+            this;
+        };
+        MyClass.prototype.minus = function (value) {
+            this._value -= value;
+            return this;
+        };
+        return MyClass;
+    }());
+    var clazz = new MyClass(10);
+    console.log(clazz.add(10).minus(5).value);
+}
--- part4/extendsimplements/interface.ts
+++ part4/extendsimplements/interface.ts
@@ -80,3 +80,22 @@
     x.hoge();
     x.foo(); // Property 'foo' does not exist on type '{ hoge(): void; }
 }
+
+{ // 型としてのthis (メソッドチェーン)
+    class MyClass {
+        constructor(private _value: number) {};
+        get value(): number {
+            return this._value;
+        }
+        add(value: number): this {
+            this._value += value;
+            returnturn this;
+        }
+        minus(value: number): this {
+            this._value -= value;
+            return this;
+        }
+    }
+    let clazz = new MyClass(10);
+    console.log(clazz.add(10).minus(5).value);
+}
